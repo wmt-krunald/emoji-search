e1c73ab588e4a055e6babf6e519222331c458966
wmt-krunald <wmt-krunald@users.noreply.github.com>
2022-03-17 08:23:47 +0000

    deploy: 6c08e08b368d0fdd6dd6fbdb620b254b2bc5f713

---
+++ precache-manifest.fc9c0ef8a7e3152f47096128e4c9ef2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd04a8df51948ff9c9108e3c83360a95",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "022c22ceed0749b0ae55",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "675730a77299aa8baf07",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "022c22ceed0749b0ae55",
+    "url": "/emoji-search/static/js/2.9f764114.chunk.js"
+  },
+  {
+    "revision": "2ce1509b9f5d601b23dc0321a205137e",
+    "url": "/emoji-search/static/js/2.9f764114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675730a77299aa8baf07",
+    "url": "/emoji-search/static/js/main.ad81996d.chunk.js"
+  },
+  {
+    "revision": "d44d1531576180c634c7",
+    "url": "/emoji-search/static/js/runtime-main.d8cd5ee4.js"
+  }
+]);
